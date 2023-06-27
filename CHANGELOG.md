3563ab920b2ea47ca862f60665d355a67fa6ff45
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-06-27 11:08:25 +0200

    Released 0.1.2.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,14 @@
+# 0.1.2
+- added nixy_markdown_controller as dependency
+- ignored .failed_tracker
+- fix: new line
+- a
+- chore: update readme
+- chore: update lisence
+- chore: publish
+- chore: publish 0.1.0
+- first upload
+
 ## 0.1.1
 
 * Add headers(h1..), list, checkbox list
--- lib/nixi_markdown.dart
+++ lib/nixi_markdown.dart
@@ -1,4 +1,5 @@
 library nixi_markdown;
 
-export 'package:nixi_markdown/formatters/newline_formatter.dart';
-export 'package:nixi_markdown/toolbar.dart';
+export 'package:nixy_markdown/formatters/newline_formatter.dart';
+export 'package:nixy_markdown/toolbar.dart';
+export 'package:nixy_markdown_controller/nixy_markdown_controller.dart';
